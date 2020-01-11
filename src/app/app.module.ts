2d97c1d049fc8095eb0d4da11347cb53fccffb21
AndresHMosqueda <andreshm94@gmail.com>
2020-01-10 22:57:55 -0600

    Effects and app finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {  HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 
+//NGRx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './store/app.reducer';
+import { effectsArr } from './store/effects/index';
+import { EffectsModule } from '@ngrx/effects';
+
+//Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Modulos personalizados
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+
+//Environment
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +29,12 @@ import { UsuariosModule } from './usuarios/usuarios.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot(effectsArr),
     SharedModule,
     UsuariosModule
   ],
